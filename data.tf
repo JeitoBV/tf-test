7bd3569ac1818cea9daf552b10ed477a7c60a997
Frank Spijkerman <frank@jeito.nl>
2024-04-26 12:10:42 +0200

    Stap 2a

--- data.tf
+++ data.tf
@@ -1,3 +1,28 @@
 data "vsphere_datacenter" "datacenter" {
   name = "DC-Huijbregts"
 }
+
+data "vsphere_datastore" "datastore" {
+  name          = "VMFS11-NoSnapShot"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_host" "host" {
+  name          = "esx-vb-03.huijbregts.com"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "demo" {
+  name          = "DEMO"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_content_library" "library" {
+  name = "HGDevops"
+}
+
+data "vsphere_content_library_item" "rhel9" {
+  name       = "linux-rhel-9.2-v23.11"
+  type       = "vm-template"
+  library_id = data.vsphere_content_library.library.id
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "vsphere_server" {
+  value = var.vsphere_server
+}
+
 output "vsphere_datacenter" {
   value = data.vsphere_datacenter.datacenter.name
 }
